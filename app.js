ddd31af4826de15bce19b020f6221614111af167
Jane <ngaukonojane@gmail.com>
2018-03-28 16:45:46 +0300

    create app layout

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-console.log('app loaded')
+const form = document.querySelector('form')
 
-const div = document.querySelector('.container')
+const taskInput = document.querySelector('input[name=taskName]')
 
-console.log(div)
+const dateInput = document.querySelector('input[name=taskDate]')
+
+const ul = document.querySelector('ul')
+
+form.onsubmit = function(e){
+	e.preventDefault()
+
+	let taskTitle = taskInput.value
+	let taskDate = dateInput.value
+
+	const li = document.createElement('li')
+	li.textContent = taskTitle
+	li.classList.add('list-group-item')
+	li.classList.add('d-flex')
+	li.classList.add('justify-content-between')
+	li.classList.add('align-items-center')
+	
+
+    const span = document.createElement('span')
+    span.textContent = taskDate
+    span.classList.add('badge')
+    span.classList.add('badge-primary')
+    span.classList.add('badge-pill')
+
+    li.appendChild(span)
+	ul.appendChild(li)
+}
